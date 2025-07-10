c4dd28cb6a278fe0c38d0addd45d594d48309d9e
trini <trinitylklein@outlook.com>
2025-07-10 16:27:12 -0500

    Update playwright tests, and workflows

--- tests/test-page-load.spec.js
+++ tests/test-page-load.spec.js
@@ -1,16 +1,26 @@
 import { test, expect } from '@playwright/test';
 
 test('Homepage loads and visitor data appears (Hugo Console Theme)', async ({ page }) => {
-  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/');
+  /* Check that resume.html contents load as expected */
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/resume.html');
 
   const title = await page.title();
-  console.log('/', title);
-  expect(title.length).toBeLessThanOrEqual(1);
+  console.log('Trinity Klein/', title);
+  expect(title.length).toBeLessThanOrEqual(14);
 
   await expect(page.getByRole('heading', { name: /About/i })).toBeVisible();
-   await expect(page.getByRole('heading', { name: /Education/i })).toBeVisible();
-    await expect(page.getByRole('heading', { name: /Relevant Experience/i })).toBeVisible();
-     await expect(page.getByRole('heading', { name: /Technical Skills & Projects/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Education/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Relevant Experience/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Technical Skills & Projects/i })).toBeVisible();
+
+  /* Check that index.html contents, mostly visitor api, load as expected */
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/');
+  await expect(page.getByRole('heading', { name: /About/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Feel Free to Reach Out To Me!/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /Latest Blogs/i })).toBeVisible();
+
+  await expect(page.locator('#unique_visitors')).not.toHaveText('Loading...', { timeout: 10000 });
+  await expect(page.locator('#total_visits')).not.toHaveText('Loading...', { timeout: 10000 });
 
   await expect(page.locator('#unique_visitors')).toBeVisible();
   await expect(page.locator('#total_visits')).toBeVisible();
@@ -18,5 +28,7 @@ test('Homepage loads and visitor data appears (Hugo Console Theme)', async ({ pa
   const unique = await page.locator('#unique_visitors').innerText();
   const total = await page.locator('#total_visits').innerText();
 
-  console.log(`Unique Visitors: ${unique}, Total Visits: ${total}`);
+  console.log('Unique visitors:', unique);
+  console.log('Total visitors:', total);
+  
 });
