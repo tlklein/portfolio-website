3004e573b23d57393001bcffde927f3f61a1f88d
trini <trinitylklein@outlook.com>
2025-07-10 15:51:33 -0500

    Update tests to cloudfront dist

--- tests/test-console-errors.spec.js
+++ tests/test-console-errors.spec.js
@@ -3,7 +3,7 @@ const { test, expect, devices } = require('@playwright/test');
 test.use({ ...devices['iPhone 13'] });
 
 test('Mobile layout loads correctly', async ({ page }) => {
-  await page.goto('http://localhost:1313/');
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/');
 
   await expect(page.locator('nav')).toBeVisible(); // or hamburger menu
   await expect(page.locator('#unique_visitors')).toBeVisible();
