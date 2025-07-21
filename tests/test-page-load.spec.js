87c9d5c36ee506af0d45491f7b4485093c8e1a49
trini <trinitylklein@outlook.com>
2025-07-21 14:39:46 -0500

    Update baseURLs in projects.html to fix appearances, update test-page-load to be current with file

--- tests/test-page-load.spec.js
+++ tests/test-page-load.spec.js
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('Homepage loads and visitor data appears (Hugo Console Theme)', async ({ page }) => {
-  /* Check that resume.html contents load as expected */
+
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/posts.html', { waitUntil: 'networkidle' });
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/projects.html', { waitUntil: 'networkidle' });
   await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/resume.html', { waitUntil: 'networkidle' });
+  await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/ping.html', { waitUntil: 'networkidle' });
 
   const title = await page.title();
-  console.log('Trinity Klein/', title);
+  console.log('trinity-klein/', title);
   expect(title.length).toBeLessThanOrEqual(14);
 
   await expect(page.getByRole('heading', { name: /About/i })).toBeVisible();
@@ -16,7 +19,7 @@ test('Homepage loads and visitor data appears (Hugo Console Theme)', async ({ pa
   /* Check that index.html contents, mostly visitor api, load as expected */
   await page.goto('https://d2d06xlq6t9xmp.cloudfront.net/', { waitUntil: 'networkidle' });
   await expect(page.getByRole('heading', { name: /About/i })).toBeVisible();
-  await expect(page.getByRole('heading', { name: /Feel Free to Reach Out To Me!/i })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /If you have any questions or inquiries for me, please feel to reach out!/i })).toBeVisible();
   await expect(page.getByRole('heading', { name: /Latest Blogs/i })).toBeVisible();
 
   await expect(page.locator('#unique_visitors')).not.toHaveText('Loading...', { timeout: 10000 });
