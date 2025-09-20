29909d3af2f4330d5f013644b1f4dd0d1d892967
trini <trinitylklein@outlook.com>
2025-09-19 23:36:30 -0500

    Change tests

--- tests/test-console-errors.spec.js
+++ tests/test-console-errors.spec.js
@@ -5,6 +5,6 @@ test.use({ ...devices['iPhone 13'] });
 test('Mobile layout loads correctly', async ({ page }) => {
   await page.goto('https://www.trinityklein.dev/', { waitUntil: 'networkidle' });
 
-  await expect(page.locator('nav')).toBeVisible(); // or hamburger menu
+  await expect(page.locator('nav')).toBeVisible();
   await expect(page.locator('#unique_visitors')).toBeVisible();
 });
