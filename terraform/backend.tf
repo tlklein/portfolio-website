24000f29741ad4aa0f314d13b662f1ede31eb0f0
trini <trinitylklein@outlook.com>
2025-10-17 11:42:47 -0500

    Update backend to match main

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket       = "tlklein-portfolio-tf-state"
-    key          = "state/terraform.tfstate"
-    region       = "us-east-2"
-    encrypt      = true
+    bucket         = "tlklein-portfolio-tf-state"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
     dynamodb_table = "TerraformLocks"
-    profile      = "tlklein-test"
+    profile        = "tlklein-test"
   }
 }
