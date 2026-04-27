b44f7a9f428fd0ded2966ef35322a9d92f622e3b
trini <trinitylklein@outlook.com>
2026-04-27 13:52:13 -0500

    type exporting default config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+export default defineConfig({});
